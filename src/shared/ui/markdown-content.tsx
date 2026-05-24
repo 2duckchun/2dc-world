3a90b41f46fd5f68cdafbbc23318ff0f8d4484fb
2DC <kkts9308@gmail.com>
2026-05-24 17:52:17 +0900

    feature: add rehypeRaw, rehypeSanitize to Markdown Renderer for seeing img tag

--- src/shared/ui/markdown-content.tsx
+++ src/shared/ui/markdown-content.tsx
@@ -1,5 +1,7 @@
 import Markdown from "react-markdown"
 import rehypeHighlight from "rehype-highlight"
+import rehypeRaw from "rehype-raw"
+import rehypeSanitize from "rehype-sanitize"
 import remarkGfm from "remark-gfm"
 import { cn } from "@/shared/lib/utils"
 
@@ -19,9 +21,10 @@ export function MarkdownContent({ markdown, className }: MarkdownContentProps) {
       <Markdown
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[
+          rehypeRaw,
+          rehypeSanitize,
           [rehypeHighlight, { plainText: ["text", "txt", "plain"] }],
         ]}
-        skipHtml
       >
         {markdown}
       </Markdown>
